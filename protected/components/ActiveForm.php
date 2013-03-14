f6f372cc8fdda376b1cc8a6ed923d8b308143fa4
Pierre Klink <dev@klinks.info>
2013-03-14 22:40:24 +0100

    added ActiveFormTest

--- protected/components/ActiveForm.php
+++ protected/components/ActiveForm.php
@@ -2,9 +2,22 @@
 
 class ActiveForm extends CActiveForm
 {
+
+    /**
+     * @param CModel $model
+     * @param string $attribute
+     * @param array $htmlOptions
+     * @param bool $enableAjaxValidation
+     * @param bool $enableClientValidation
+     * @return mixed|string
+     */
     public function error($model, $attribute, $htmlOptions = array(), $enableAjaxValidation = true, $enableClientValidation = true)
     {
-        return str_replace(array('div', 'errorMessage'), array('small', 'error'), parent::error($model, $attribute, $htmlOptions, $enableAjaxValidation, $enableClientValidation));
+        return str_replace(
+            array('div', 'errorMessage'),
+            array('small', 'error'),
+            parent::error($model, $attribute, $htmlOptions, $enableAjaxValidation, $enableClientValidation)
+        );
     }
 
 }
