507010c6c62c7dd4806501201eeb1edb1f809d95
Pierre Klink <dev@klinks.info>
2013-07-28 18:39:58 +0200

    removed main-layout

--- protected/views/layouts/main.php
+++
@@ -1,9 +0,0 @@
-<?php $this->beginContent('application.views.layouts.master'); ?>
-
-    <div class="container-fluid">
-        <?php echo $content; ?>
-    </div>
-
-    <?php $this->renderPartial('/layouts/_modals'); ?>
-
-<?php $this->endContent(); ?>
