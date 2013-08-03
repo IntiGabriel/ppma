90ed92937a41340930d669687546bd3476eb3b80
Pierre Klink <dev@klinks.info>
2013-08-03 15:01:45 +0200

    removed app.js

--- js/ppma/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    'use strict';
-
-    ppma.Collection.Entries.fetch();
-
-    // add shortcuts
-    keymage('alt-c', $.proxy(ppma.View.Entry.Modal.show, ppma.View.Entry.Modal));
-
-});
