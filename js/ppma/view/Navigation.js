c7f817583d895184890975657a3e6bb6d6cdc9e2
Pierre Klink <dev@klinks.info>
2013-08-05 15:25:34 +0200

    added fade-effect to navigation

--- js/ppma/view/Navigation.js
+++ js/ppma/view/Navigation.js
@@ -8,7 +8,13 @@ $(function() {
         events: {
             'click .show-entry-modal':    function() { ppma.View.Entry.Content.create(); return false; },
             'click .show-password-modal': function() { ppma.View.Settings.Modal.show(); return false; }
+        },
+
+
+        initialize:  function() {
+            this.$el.hide().fadeIn('slow');
         }
+
 
     });
 
